b8679592f0b150a44858010947c76e2bc0a90e3c
serhatbek <serhat.bekk@gmail.com>
2022-08-29 11:11:31 +0300

    error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <section className='section'>
+      <h2>404</h2>
+      <p>Page not found</p>
+      <Link to='/'>Back Home</Link>
+    </section>
+  );
 };
 
 export default Error;
