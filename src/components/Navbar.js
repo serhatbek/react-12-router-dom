6ebffd1a50e25c65e1f185898f9c8c935d44bb67
serhatbek <serhat.bekk@gmail.com>
2022-08-29 12:56:47 +0300

    NavLink active state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,26 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
-      <Link to='/products'>Products</Link>
+      <NavLink
+        to='/'
+        className={({ isActive }) => (isActive ? 'link active' : 'link')}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to='/about'
+        className={({ isActive }) => (isActive ? 'link active' : 'link')}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to='/products'
+        className={({ isActive }) => (isActive ? 'link active' : 'link')}
+      >
+        Products
+      </NavLink>
     </nav>
   );
 };
