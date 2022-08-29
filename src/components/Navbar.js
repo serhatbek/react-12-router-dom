e87654121c904558332a34f74272ff5701cb1968
serhatbek <serhat.bekk@gmail.com>
2022-08-29 12:41:56 +0300

    shared layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='navbar'>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/products'>Products</Link>
+    </nav>
+  );
 };
 
 export default Navbar;
