c43e89fdddc3cd0840dbb7b7ae9adc309de59194
serhatbek <serhat.bekk@gmail.com>
2022-08-29 09:02:20 +0300

    project setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
 
 function App() {
-  return <div className='App'>router</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='products' element={<Products />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
